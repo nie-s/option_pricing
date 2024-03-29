6f71391c0fbea5d0beaf95846275567d194eac14
DanisHaiguerre <77338799+DanisHaiguerre@users.noreply.github.com>
2024-03-30 00:59:12 +0800

    Update TestCaseAmerican.py
    测试具体数据没想好怎么设计

--- TestCaseAmerican.py
+++ TestCaseAmerican.py
@@ -3,11 +3,9 @@ import numpy as np
 from BionomalTreeAmerican import BionomalTreeAmerican  
   
 # Fix the parameters   
-S = 50
 sigma = 0.4
 r = 0.1
 n = 200
-T = 2
   
 # Set the random seed for reproducibility  
 np.random.seed(42)  
@@ -23,8 +21,8 @@ class TestMonteCarloAmericaOptions(unittest.TestCase):
         for s0_i, K_i, T_i in zip(s0, K, T):  
             america_price = BionomalTreeAmerican(s0_i, sigma, r, T_i, K_i, n, 'put')  
         
-            C = america_price.american_fast_tree()  
-            self.assertGreaterEqual(C, 0, f"Bionomal Tree American put option price should be non-negative for s0={s0_i}, K={K_i}, T={T_i}")
+            S = america_price.american_fast_tree()  
+            self.assertGreaterEqual(S, 0, f"Bionomal Tree American put option price should be non-negative for s0={s0_i}, K={K_i}, T={T_i}")
             
           
     def test_call_america(self):  
@@ -36,8 +34,8 @@ class TestMonteCarloAmericaOptions(unittest.TestCase):
         for s0_j, K_j, T_j in zip(s0, K, T):  
             america_price = BionomalTreeAmerican(s0_j, sigma, r, T_j, K_j, n, 'call')  
         
-            C = america_price.american_fast_tree()  
-            self.assertGreaterEqual(C, 0, f"Bionomal Tree American call option price should be non-negative for s0={s0_j}, K={K_j}, T={T_j}")
+            S = america_price.american_fast_tree()  
+            self.assertGreaterEqual(S, 0, f"Bionomal Tree American call option price should be non-negative for s0={s0_j}, K={K_j}, T={T_j}")
                 
             
 if __name__ == '__main__':  
