de4cf311d493b9d8bf1900c7fdfde105a2295ab0
DanisHaiguerre <77338799+DanisHaiguerre@users.noreply.github.com>
2024-03-30 20:39:37 +0800

    Update TestCaseQMC.py
    3.3294557543634675 3.6110953650454936
    4.779377174719182 0.7728451462442409
    4.545816330899413 10.08343106326392
    2.668002847041171 22.998942270221626
    3.596242808318504 16.066005858621647

--- TestCaseQMC.py
+++ TestCaseQMC.py
@@ -2,13 +2,13 @@ import unittest
 import numpy as np  
 from QMonteCarloKIKOPut import QMonteCarloKIKOPut  
   
+
 # Fix the parameters  
 r = 0.06  
+R = 5
+m = 100000  # Number of paths
 T = 1.0  
 n = 10
-R = 5
-
-m = 100000  # Number of paths 
 sigma0 = 0.1
 
 
@@ -19,10 +19,10 @@ class TestQMC(unittest.TestCase):
       
     def test_qmc(self):  
         # Test put options
-        s0 = [10, 10, 10] 
-        K = [9, 9, 9]
-        L = [7, 8, 9]
-        U = [11, 12, 13]
+        s0 = [9.5, 10, 10.5, 9.8, 10.2] 
+        K = [9, 9, 9, 9, 9]
+        L = [9.3, 9.6, 10.1, 9.5, 9.8]
+        U = [10.2, 10.4, 10.9, 10.1, 10.6]
          
         
         for s0_i, K_i, L_i, U_i in zip(s0, K, L, U):  
@@ -33,7 +33,9 @@ class TestQMC(unittest.TestCase):
             self.assertGreaterEqual(delta, 0, f"QMC put option delta should be non-negative for s0={s0_i}, K={K_i}, L={L_i}, U={U_i}")  
  
       
-    
+if __name__ == '__main__':  
+    unittest.main()
+
             
 if __name__ == '__main__':  
     unittest.main()
