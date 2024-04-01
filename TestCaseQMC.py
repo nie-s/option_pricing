d5afa88098bfe2b343335ca83f1fc5a0509d654b
DanisHaiguerre <77338799+DanisHaiguerre@users.noreply.github.com>
2024-04-01 21:02:27 +0800

    Update TestCaseQMC.py

--- TestCaseQMC.py
+++ TestCaseQMC.py
@@ -1,42 +1,133 @@
-import unittest  
-import numpy as np  
-from QMonteCarloKIKOPut import QMonteCarloKIKOPut  
-  
+import os
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
-# Fix the parameters  
-r = 0.06  
+from QMonteCarloKIKOPut import QMonteCarloKIKOPut   
+title = 'QMA KIKO Put'
+
+
+"""
+
+How Current Price Affects Put Option price
+
+"""
+
+K = 9
+L = 9.3
+U = 10.2
 R = 5
-m = 100000  # Number of paths
-T = 1.0  
-n = 10
+r = 0.06
 sigma0 = 0.1
+T = 1.0
+m = 10**5
+n = 10
+price=[]
+param=[]
+for s0 in range(95,105,1):
+    s0 = s0 * 0.1
+    C = QMonteCarloKIKOPut(s0, K, L, U, R, r, sigma0, T, m, n)
+    price.append(C.calculate_price_and_delta())
+    param.append(s0)
+
+plt.plot(param,price)
+# naming the x axis
+plt.xlabel('Current Price')
+# naming the y axis
+plt.ylabel('KIKO Put Option Price')
+plt.title(title+' Option')
+plt.show()
 
 
-# Set the random seed for reproducibility  
-np.random.seed(42)  
+"""
 
-class TestQMC(unittest.TestCase):  
-      
-    def test_qmc(self):  
-        # Test put options
-        s0 = [9.5, 10, 10.5, 9.8, 10.2] 
-        K = [9, 9, 9, 9, 9]
-        L = [9.3, 9.6, 10.1, 9.5, 9.8]
-        U = [10.2, 10.4, 10.9, 10.1, 10.6]
-         
-        
-        for s0_i, K_i, L_i, U_i in zip(s0, K, L, U):  
-            qmc_test = QMonteCarloKIKOPut(s0_i, K_i, L_i, U_i, R, r, sigma0, T, m, n)  
-        
-            option_price,delta = qmc_test.calculate_price_and_delta()   
-            self.assertGreaterEqual(option_price, 0, f"QMC put option price should be non-negative for s0={s0_i}, K={K_i}, L={L_i}, U={U_i}")
-            self.assertGreaterEqual(delta, 0, f"QMC put option delta should be non-negative for s0={s0_i}, K={K_i}, L={L_i}, U={U_i}")  
- 
-      
-if __name__ == '__main__':  
-    unittest.main()
+How Maturity Affects Put Option Price
 
-            
-if __name__ == '__main__':  
-    unittest.main()
+"""
 
+s0 = 10
+K = 9
+L = 9.3
+U = 10.2
+R = 5
+r = 0.06
+sigma0 = 0.1
+m = 10**5
+n = 10
+price=[]
+param=[]
+for T in range(1,40,1):
+    T = T * 0.2
+    C = QMonteCarloKIKOPut(s0, K, L, U, R, r, sigma0, T, m, n)
+    price.append(C.calculate_price_and_delta())
+    param.append(T)
+plt.plot(param,price)
+# naming the x axis
+plt.xlabel('Maturity')
+# naming the y axis
+plt.ylabel('KIKO Put Option Price')
+plt.title(title+ ' Option')
+plt.show()
+
+
+"""
+
+How Risk Free Rate Affects Put Option Price
+
+"""
+
+s0 = 10
+K = 9
+L = 9.3
+U = 10.2
+R = 5
+sigma0 = 0.1
+m = 10**5
+n = 10
+price=[]
+param=[]
+for r in range(1,9,1):
+    r = r * 0.01
+    C = QMonteCarloKIKOPut(s0, K, L, U, R, r, sigma0, T, m, n)
+    price.append(C.calculate_price_and_delta())
+    param.append(r)
+plt.plot(param,price)
+# naming the x axis
+plt.xlabel('Risk Free Rate')
+# naming the y axis
+plt.ylabel('KIKO Put Option Price')
+plt.title(title+ ' Option')
+plt.show()
+
+
+"""
+
+How Number of Observation Times Affects Put Option Price
+
+"""
+
+s0 = 10
+K = 9
+L = 9.3
+U = 10.2
+R = 5
+r = 0.06
+sigma0 = 0.1
+m = 10**5
+price=[]
+param=[]
+for n in range(10,500):
+    C = QMonteCarloKIKOPut(s0, K, L, U, R, r, sigma0, T, m, n)
+    price.append(C.calculate_price_and_delta())
+    param.append(n)
+plt.plot(param,price)
+# naming the x axis
+plt.xlabel('Observation Time Steps')
+# naming the y axis
+plt.ylabel('KIKO Put Option Price')
+plt.title(title+ ' Option')
+plt.show()
