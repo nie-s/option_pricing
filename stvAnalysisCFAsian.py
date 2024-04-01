b8add2bd3b6f634cdb2899544395df10f67600c3
DanisHaiguerre <77338799+DanisHaiguerre@users.noreply.github.com>
2024-04-01 14:09:03 +0800

    Update stvAnalysisCFAsian.py

--- stvAnalysisCFAsian.py
+++ stvAnalysisCFAsian.py
@@ -7,7 +7,7 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from MonteCarloAsianVC import MonteCarloAsianVC   
+from ClosedFormAsian import ClosedFormAsian   
 title = 'Closed Form Asian'
 
 
@@ -23,12 +23,11 @@ T = 0.5
 sigma = 0.2
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for K in range(10,100,10):
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(K)
 plt.plot(param,price)
 # naming the x axis
@@ -51,12 +50,11 @@ T = 0.5
 sigma = 0.2
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for K in range(10,100,10):
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(K)
 plt.plot(param,price)
 # naming the x axis
@@ -79,13 +77,12 @@ t = 0
 sigma = 0.2
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for T in range(1,40,1):
     T = T * 0.2
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(T)
 plt.plot(param,price)
 # naming the x axis
@@ -108,13 +105,12 @@ t = 0
 sigma = 0.2
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for T in range(1,40,1):
     T = T * 0.2
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(T)
 plt.plot(param,price)
 # naming the x axis
@@ -142,13 +138,12 @@ t = 0
 T = 0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(sigma)   
 ax[0].plot(param,price, label = 'ITM', color='r')
 ax[0].set_ylabel('Call Option price')
@@ -159,13 +154,12 @@ t = 0
 T=0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(sigma)
 ax[1].plot(param,price, label = 'ATM', color='b')
 ax[1].set_ylabel('Call Option price')
@@ -176,13 +170,12 @@ t = 0
 T=0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma  * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(sigma)
 ax[2].plot(param,price, label='OTM',color='g')
 ax[2].set_ylabel('Call Option price')
@@ -209,13 +202,12 @@ t = 0
 T = 0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(sigma)   
 ax[0].plot(param,price, label = 'ITM', color='r')
 ax[0].set_ylabel('Call Option price')
@@ -226,13 +218,12 @@ t = 0
 T=0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(sigma)
 ax[1].plot(param,price, label = 'ATM', color='b')
 ax[1].set_ylabel('Put Option price')
@@ -243,13 +234,12 @@ t = 0
 T=0.5
 r = 0.01
 n = 50
-m = 10**5
 price=[]
 param=[]
 for sigma in range(1,100,1):
     sigma = sigma * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(sigma)
 ax[2].plot(param,price, label='OTM',color='g')
 ax[2].set_ylabel('Put Option price')
@@ -271,13 +261,12 @@ t = 0
 T = 0.5
 sigma = 0.2
 n = 50
-m = 10**5
 price=[]
 param=[]
 for r in range(1,40,1):
     r = r * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'call', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'call')
+    price.append(C.cf_asian_call())
     param.append(r)
 plt.plot(param,price)
 # naming the x axis
@@ -300,13 +289,12 @@ t = 0
 T = 0.5
 sigma = 0.2
 n = 50
-m = 10**5
 price=[]
 param=[]
 for r in range(1,40,1):
     r = r * 0.01
-    C = MonteCarloAsianVC(s0, sigma, r, T, K, n, m, 'put', True)
-    price.append(C.asian_geo())
+    C = ClosedFormAsian(s0, sigma, r, T, K, n, 'put')
+    price.append(C.cf_asian_put())
     param.append(r)
 plt.plot(param,price)
 # naming the x axis
